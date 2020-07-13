c95fd9e19fdde020ab2a4adf03c79994b987cd7b
Chayce Knaub <chknaub@yahoo.com>
2020-07-13 19:35:11 -0400

    api refactor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import AddBookmark from './AddBookmark/AddBookmark';
-import BookmarkList from './BookmarkList/BookmarkList';
-import BookmarksContext from './BookmarksContext';
-import Nav from './Nav/Nav';
-import config from './config';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import AddBookmark from "./AddBookmark/AddBookmark";
+import EditBookmark from "./EditBookmark/EditBookmark";
+import BookmarkList from "./BookmarkList/BookmarkList";
+import BookmarksContext from "./BookmarksContext";
+import Nav from "./Nav/Nav";
+import config from "./config";
+import "./App.css";
 
 class App extends Component {
   state = {
@@ -13,69 +14,73 @@ class App extends Component {
     error: null,
   };
 
-  setBookmarks = bookmarks => {
+  setBookmarks = (bookmarks) => {
     this.setState({
       bookmarks,
       error: null,
-    })
-  }
+    });
+  };
 
-  addBookmark = bookmark => {
+  addBookmark = (bookmark) => {
     this.setState({
-      bookmarks: [ ...this.state.bookmarks, bookmark ],
-    })
-  }
+      bookmarks: [...this.state.bookmarks, bookmark],
+    });
+  };
 
-  deleteBookmark = bookmarkId => {
-    console.log(bookmarkId)
-    // todo: remove bookmark with bookmarkId from state
-    const newBookmarks = this.state.bookmarks.filter(bm =>
-      bm.id !== bookmarkId
-    )
+  deleteBookmark = (bookmarkId) => {
+    const newBookmarks = this.state.bookmarks.filter(
+      (bm) => bm.id !== bookmarkId
+    );
     this.setState({
-      bookmarks: newBookmarks
-    })
-  }
+      bookmarks: newBookmarks,
+    });
+  };
 
   componentDidMount() {
     fetch(config.API_ENDPOINT, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'content-type': 'application/json',
-        'Authorization': `Bearer ${config.API_KEY}`
-      }
+        "content-type": "application/json",
+        Authorization: `Bearer ${config.API_KEY}`,
+      },
     })
-      .then(res => {
+      .then((res) => {
         if (!res.ok) {
-          throw new Error(res.status)
+          return res.json().then((error) => Promise.reject(error));
         }
-        return res.json()
+        return res.json();
       })
       .then(this.setBookmarks)
-      .catch(error => this.setState({ error }))
+      .catch((error) => {
+        console.error(error);
+        this.setState({ error });
+      });
   }
+
+  updateBookmark = (updatedBookmark) => {
+    this.setState({
+      bookmarks: this.state.bookmarks.map((bm) =>
+        bm.id !== updatedBookmark.id ? bm : updatedBookmark
+      ),
+    });
+  };
 
   render() {
     const contextValue = {
       bookmarks: this.state.bookmarks,
       addBookmark: this.addBookmark,
       deleteBookmark: this.deleteBookmark,
-    }
+      updateBookmark: this.updateBookmark,
+    };
     return (
-      <main className='App'>
+      <main className="App">
         <h1>Bookmarks!</h1>
         <BookmarksContext.Provider value={contextValue}>
           <Nav />
-          <div className='content' aria-live='polite'>
-            <Route
-              path='/add-bookmark'
-              component={AddBookmark}
-            />
-            <Route
-              exact
-              path='/'
-              component={BookmarkList}
-            />
+          <div className="content" aria-live="polite">
+            <Route exact path="/" component={BookmarkList} />
+            <Route path="/add-bookmark" component={AddBookmark} />
+            <Route path="/edit/:bookmarkId" component={EditBookmark} />
           </div>
         </BookmarksContext.Provider>
       </main>
--- src/BookmarkItem/BookmarkItem.js
+++ src/BookmarkItem/BookmarkItem.js
@@ -1,64 +1,54 @@
-import React from 'react';
-import Rating from '../Rating/Rating';
-import BookmarksContext from '../BookmarksContext';
-import config from '../config';
-import './BookmarkItem.css';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import Rating from "../Rating/Rating";
+import BookmarksContext from "../BookmarksContext";
+import config from "../config";
+import "./BookmarkItem.css";
 
 function deleteBookmarkRequest(bookmarkId, cb) {
   fetch(config.API_ENDPOINT + `/${bookmarkId}`, {
-    method: 'DELETE',
+    method: "DELETE",
     headers: {
-      'content-type': 'application/json',
-      'authorization': `bearer ${config.API_KEY}`
-    }
+      "content-type": "application/json",
+      authorization: `bearer ${config.API_KEY}`,
+    },
   })
-    .then(res => {
+    .then((res) => {
       if (!res.ok) {
-        // get the error message from the response,
-        return res.json().then(error => {
-          // then throw it
-          throw error
-        })
+        return res.json().then((error) => Promise.reject(error));
       }
-      return res.json()
+      return res.json();
     })
-    .then(data => {
-      console.log({ data })
-      cb(bookmarkId)
+    .then((data) => {
+      cb(bookmarkId);
     })
-    .catch(error => {
-      console.log(error)
-    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
 export default function BookmarkItem(props) {
   return (
     <BookmarksContext.Consumer>
       {(context) => (
-        <li className='BookmarkItem'>
-          <div className='BookmarkItem__row'>
-            <h3 className='BookmarkItem__title'>
-              <a
-                href={props.url}
-                target='_blank'
-                rel='noopener noreferrer'>
+        <li className="BookmarkItem">
+          <div className="BookmarkItem__row">
+            <h3 className="BookmarkItem__title">
+              <a href={props.url} target="_blank" rel="noopener noreferrer">
                 {props.title}
               </a>
             </h3>
             <Rating value={props.rating} />
           </div>
-          <p className='BookmarkItem__description'>
-            {props.description}
-          </p>
-          <div className='BookmarkItem__buttons'>
+          <p className="BookmarkItem__description">{props.description}</p>
+          <div className="BookmarkItem__buttons">
+            <Link to={`/edit/${props.id}`}>Edit</Link>{" "}
             <button
-              className='BookmarkItem__description'
-              onClick={() => {
-                deleteBookmarkRequest(
-                  props.id,
-                  context.deleteBookmark,
-                )
-              }}
+              className="BookmarkItem__description"
+              onClick={() =>
+                deleteBookmarkRequest(props.id, context.deleteBookmark)
+              }
             >
               Delete
             </button>
@@ -66,9 +56,18 @@ export default function BookmarkItem(props) {
         </li>
       )}
     </BookmarksContext.Consumer>
-  )
+  );
 }
 
 BookmarkItem.defaultProps = {
   onClickDelete: () => {},
-}
+};
+
+BookmarkItem.propTypes = {
+  id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  desciption: PropTypes.string,
+  rating: PropTypes.number.isRequired,
+  onClickDelete: PropTypes.func,
+};
